a60314f1592d969e5d1a1ac63db4ce22f11dfd5c
tessG <tess@lovegreen.life>
2026-03-10 18:36:54 +0100

    front-end starter coded

--- src/main/java/CarpoolApp.java
+++ src/main/java/CarpoolApp.java
@@ -1,6 +1,7 @@
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.File;
 import java.util.*;
 
@@ -8,14 +9,18 @@ public class CarpoolApp {
 
     static final File FILE = new File("rides.json");
     static final ObjectMapper mapper = new ObjectMapper();
+    static List<String> messages = new ArrayList<>();
+
 
     public static void main(String[] args) throws Exception {
+        messages.add("Hello world");
 
-        if (!FILE.exists()){
+        if (!FILE.exists()) {
             mapper.writeValue(FILE, new ArrayList<>());
         }
 
         Javalin app = Javalin.create(config -> {
+            //brug denne linje for at undgå at genstarte server når statiske sider er ændret
             config.staticFiles.add(System.getProperty("user.dir") + "/src/main/resources/public", Location.EXTERNAL);
 
             //config.staticFiles.add("/public", Location.CLASSPATH);
@@ -25,21 +30,26 @@ public class CarpoolApp {
             List<Map> rides = mapper.readValue(FILE, List.class);
             //Her bliver json data konverteret til en java datastruktur (Map)
             rides.add(ctx.bodyAsClass(Map.class));
+
             //som så sendes videre til mapper objektet, for at blive skrevet til serveres filssystem
             mapper.writeValue(FILE, rides);
             ctx.status(201);
         });
 
         app.get("/rides", ctx -> {
-            List<Map> rides = mapper.readValue(FILE, List.class);
+           ctx.json(messages);
+
+
+            //mapper læser JSON og laver det om til en Java data struktur vi kender
+            /*List<Map> rides = mapper.readValue(FILE, List.class);
             StringBuilder html = new StringBuilder("<table>");
             html.append("<tr><th>Navn</th><th>Dato</th><th>Fra</th><th>Til</th><th>Kommentarer</th><th>Rygning</th></tr>");
 
-        for (Map ride : rides) {
-               // html.append("<tr>")
-            String color = "driver".equals(ride.get("role")) ? "steelblue" : "orange";
-            html.append("<tr style='border-left: 6px solid ").append(color).append("'>")
-                    .append("<td>").append(ride.get("name")).append("</td>")
+            for (Map ride : rides) {
+                // html.append("<tr>")
+                String color = "driver".equals(ride.get("role")) ? "steelblue" : "orange";
+                html.append("<tr style='border-left: 6px solid ").append(color).append("'>")
+                        .append("<td>").append(ride.get("name")).append("</td>")
                         .append("<td>").append(ride.get("date")).append("</td>")
                         .append("<td>").append(ride.get("from")).append("</td>")
                         .append("<td>").append(ride.get("to")).append("</td>")
@@ -47,9 +57,11 @@ public class CarpoolApp {
                         .append("<td>").append(ride.get("smoking")).append("</td>")
                         .append("</tr>");
             }
-
             html.append("</table>");
-            ctx.html(html.toString());
+           ctx.html(html.toString());
+
+*/
+
         });
 
         app.start(7001);
